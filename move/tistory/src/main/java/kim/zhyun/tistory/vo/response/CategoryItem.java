6fe4d441d2fa42010a933295ffc96c5499dc08b9
zhyunk <gimwlgus@gmail.com>
2023-11-17 12:20:16 +0900

    fix : category response vo
    데이터 하나가 배열로 들어오는 알아차리지 못하고 만들어 놓아서 바로 잡음

--- move/tistory/src/main/java/kim/zhyun/tistory/vo/response/CategoryItem.java
+++ move/tistory/src/main/java/kim/zhyun/tistory/vo/response/CategoryItem.java
@@ -3,9 +3,11 @@ package kim.zhyun.tistory.vo.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter @Setter
 public class CategoryItem {
 
-    private CategoryRealItem categories;
+    private ArrayList<CategoryRealItem> categories = new ArrayList<>();
 
 }
