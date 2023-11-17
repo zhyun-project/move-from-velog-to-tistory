dbbe657e27a5783ac170a94eedcd8ce7ba995845
zhyunk <gimwlgus@gmail.com>
2023-11-18 01:42:27 +0900

    refactor : tistory module - feign client

--- move/tistory/src/main/java/kim/zhyun/tistory/service/impl/TistoryServiceImpl.java
+++ move/tistory/src/main/java/kim/zhyun/tistory/service/impl/TistoryServiceImpl.java
@@ -3,6 +3,7 @@ package kim.zhyun.tistory.service.impl;
 import kim.zhyun.tistory.client.TistoryClient;
 import kim.zhyun.tistory.service.TistoryService;
 import kim.zhyun.tistory.vo.Response;
+import kim.zhyun.tistory.vo.request.RequestPostWrite;
 import kim.zhyun.tistory.vo.response.BlogInfoFromTistory;
 import kim.zhyun.tistory.vo.response.CategoryFromTistory;
 import kim.zhyun.tistory.vo.response.PhotoFromTistory;
@@ -30,38 +31,57 @@ public class TistoryServiceImpl implements TistoryService {
         return tistoryClient.blogInfo(accessToken, output);
     }
 
-    public Response<PostFromTistory> postUpload() {
-        // TODO: 기능 구현 해야 됨 
-        Response<PostFromTistory> response = tistoryClient.postUpload(accessToken,
-                blogName,
-                "",
-                "",
-                3,
-                "",
-                "",
-                "",
-                "",
-                1);
+    @Override
+    public Response<CategoryFromTistory> getCategory() {
+        // TODO: 기능 구현 해야 됨 > H2 저장
 
-        log.info("Post Url = {}", response.getTistory().getUrl());
-        return response;
+        return tistoryClient.getCategory(accessToken, output, blogName);
     }
 
     @Override
     public Response<PhotoFromTistory> fileUpload() {
         // TODO: 기능 구현 해야 됨 > 파일 업로드 후 H2 저장
 
+        return clientMappedFile("pathname");
+    }
+
+    public Response<PostFromTistory> postUpload() {
+        // TODO: 기능 구현 해야 됨 
+        RequestPostWrite requestPostWrite = RequestPostWrite.builder()
+                .access_token(accessToken)
+                .title("")
+                .content("")
+                .category("")
+                .slogan("") // url-slug
+                .tag("").build();
+
+        Response<PostFromTistory> response = clientMappedPostDto(requestPostWrite);
+
+        log.info("Post Url = {}", response.getTistory().getUrl());
+        return response;
+    }
+
+
+
+    private Response<PostFromTistory> clientMappedPostDto(RequestPostWrite requestPostWrite) {
+
+        return tistoryClient.postUpload(accessToken,
+                blogName,
+                requestPostWrite.getTitle(),
+                requestPostWrite.getContent(),
+                requestPostWrite.getVisibility(), // 비공개 : 0, 공개 : 3
+                requestPostWrite.getCategory(),
+                requestPostWrite.getPublished(),
+                requestPostWrite.getSlogan(),
+                requestPostWrite.getTag(),
+                1);
+    }
+
+    private Response<PhotoFromTistory> clientMappedFile(String pathname) {
+
         return tistoryClient.fileUpload("multipart/form-data",
                 accessToken, blogName,
-                new File("pathname"));
+                new File(pathname));
     }
-
-    @Override
-    public Response<CategoryFromTistory> getCategory() {
-        // TODO: 기능 구현 해야 됨 > H2 저장
-        
-        return tistoryClient.getCategory(accessToken, output, blogName);
-    }
-
 
 }
