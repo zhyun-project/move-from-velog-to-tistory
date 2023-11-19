e7efb8542db29e19112e6e92e1a1920e7190a697
zhyunk <gimwlgus@gmail.com>
2023-11-20 02:14:32 +0900

    feat : tistory module - add post upload scheduler
    매일 0시 1분 실행

--- move/tistory/src/main/java/kim/zhyun/tistory/controller/TistoryController.java
+++ move/tistory/src/main/java/kim/zhyun/tistory/controller/TistoryController.java
@@ -5,6 +5,7 @@ import kim.zhyun.tistory.data.vo.Response;
 import kim.zhyun.tistory.data.vo.response.BlogInfoFromTistory;
 import kim.zhyun.tistory.model.service.TistoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,9 @@ public class TistoryController {
 
     /**
      * `tistory`에 post upload
+     * - 매일 0시 1분 실행
      */
+    @Scheduled(cron = "0 1 0 * * *", zone = "Asia/Seoul")
     @GetMapping("/post-upload")
     public void postUpload() {
         tistoryService.postUpload();
