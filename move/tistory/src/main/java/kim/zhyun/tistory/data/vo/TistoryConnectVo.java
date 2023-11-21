82224ba783003d850c9702edc3d4084aa8a9a898
zhyunk <gimwlgus@gmail.com>
2023-11-22 06:19:33 +0900

    feat : tistory module - tistory connect vo
    외부 설정값 반영을 위해 @RefreshScope 추가

--- move/tistory/src/main/java/kim/zhyun/tistory/data/vo/TistoryConnectVo.java
+++ move/tistory/src/main/java/kim/zhyun/tistory/data/vo/TistoryConnectVo.java
@@ -2,9 +2,11 @@ package kim.zhyun.tistory.data.vo;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Getter
+@RefreshScope
 @Component
 public class TistoryConnectVo {
 
